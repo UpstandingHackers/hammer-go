2d62147dfee11af64782cc110bc27d0e10b86bee
Stephen Weinberg <stephen@prevoty.com>
2013-11-07 22:17:41 -0800

    add base64 test

---
+++ base64_test.go
@@ -0,0 +1,44 @@
+package hammer
+
+import (
+	"os"
+	"testing"
+)
+
+/* different than the C example because go can't use nil as a
+   const initilizer */
+var document HParser = nil
+
+func init_parser() {
+	// CORE
+	var digit HParser = Ch_range(0x30, 0x39)
+	var alpha HParser = Choice(Ch_range(0x41, 0x5a), Ch_range(0x61, 0x7a), nil)
+
+	// AUX.
+	var plus HParser = Ch('+')
+	var slash HParser = Ch('/')
+	var equals HParser = Ch('=')
+
+	var bsfdig HParser = Choice(alpha, digit, plus, slash, nil)
+	var bsfdig_4bit HParser = In([]byte("AEIMQUYcgkosw048"))
+	var bsfdig_2bit HParser = In([]byte("AQgw"))
+	var base64_3 HParser = Repeat_n(bsfdig, 4)
+	var base64_2 HParser = Sequence(bsfdig, bsfdig, bsfdig_4bit, equals, nil)
+	var base64_1 HParser = Sequence(bsfdig, bsfdig_2bit, equals, equals, nil)
+	var base64 HParser = Sequence(Many(base64_3), Optional(Choice(base64_2, base64_1, nil)), nil)
+	document = Sequence(Whitespace(base64), Whitespace(End_p()), nil)
+}
+
+func TestBase64(t *testing.T) {
+	init_parser()
+
+	input := []byte("aGk=") // "hi"
+	result := Parse(document, input)
+
+	// show result of parse actions
+	if result.ast != nil {
+		Pprint(os.Stdout, result.ast, 0, 0)
+	} else {
+		t.Error("fail")
+	}
+}
