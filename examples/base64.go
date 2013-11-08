f56dfbe058d99a4af09f27953c48aef6801113d8
Stephen Weinberg <stephen@prevoty.com>
2013-11-07 21:20:50 -0800

    some fixes to base64 example

--- examples/base64.go
+++ examples/base64.go
@@ -1,10 +1,12 @@
 package main
+
 import (
-	"hammer"
-	"io"
-	"os"
 	"bytes"
 	"fmt"
+	"io"
+	"os"
+
+	"github.com/prevoty/hammer"
 )
 
 /* different than the C example because go can't use nil as a
@@ -22,8 +24,8 @@ func init_parser() {
 	var equals hammer.HParser = hammer.Ch('=')
 
 	var bsfdig hammer.HParser = hammer.Choice(alpha, digit, plus, slash, nil)
-	var bsfdig_4bit hammer.HParser = hammer.In("AEIMQUYcgkosw048", 16)
-	var bsfdig_2bit hammer.HParser = hammer.In("AQgw", 4)
+	var bsfdig_4bit hammer.HParser = hammer.In([]byte("AEIMQUYcgkosw048"))
+	var bsfdig_2bit hammer.HParser = hammer.In([]byte("AQgw"))
 	var base64_3 hammer.HParser = hammer.Repeat_n(bsfdig, 4)
 	var base64_2 hammer.HParser = hammer.Sequence(bsfdig, bsfdig, bsfdig_4bit, equals, nil)
 	var base64_1 hammer.HParser = hammer.Sequence(bsfdig, bsfdig_2bit, equals, equals, nil)
@@ -42,15 +44,15 @@ func main() {
 	io.Copy(bytes.NewBuffer(input), os.Stdin)
 
 	// show what we got
-	inputsize = len(input)
-	fmt.Printf("inputsize=%lu\ninput=", inputsize)
+	inputsize := len(input)
+	fmt.Printf("inputsize=%d\ninput=%s\n", inputsize, input)
 
 	// parse input
-	result = hammer.Parse(document, input, inputsize)
+	result = hammer.Parse(document, input)
 
 	// show result of parse actions
 	if result {
-		fmt.Printf("parsed=%lld bytes\n", result.bit_length / 8)
+		fmt.Printf("parsed=%d bytes\n", result.bit_length/8)
 		hammer.Pprint(stdout, result.ast, 0, 0)
 		return 0
 	} else {
