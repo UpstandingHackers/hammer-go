1c74c12369d68451f94bdbd6bdff7893003524e8
Stephen Weinberg <stephen@prevoty.com>
2013-11-09 11:55:28 -0800

    Rewrite base64 test to be more idiomatic Go

--- base64_test.go
+++ base64_test.go
@@ -2,54 +2,51 @@ package hammer
 
 import (
 	"fmt"
-	"os"
 	"testing"
 )
 
-/* different than the C example because go can't use nil as a
-   const initilizer */
-var document HParser = nil
+func base64_parser() HParser {
+	digit := Ch_range('0', '9')
+	alpha := Choice(
+		Ch_range('A', 'Z'),
+		Ch_range('a', 'z'),
+	)
 
-func init_parser() {
-	// CORE
-	var digit HParser = Ch_range(0x30, 0x39)
-	var alpha HParser = Choice(Ch_range(0x41, 0x5a), Ch_range(0x61, 0x7a), nil)
+	plus := Ch('+')
+	slash := Ch('/')
+	equals := Ch('=')
 
-	// AUX.
-	var plus HParser = Ch('+')
-	var slash HParser = Ch('/')
-	var equals HParser = Ch('=')
+	bsfdig := Choice(alpha, digit, plus, slash)
+	bsfdig_4bit := In([]byte("AEIMQUYcgkosw048"))
+	bsfdig_2bit := In([]byte("AQgw"))
 
-	var bsfdig HParser = Choice(alpha, digit, plus, slash, nil)
-	var bsfdig_4bit HParser = In([]byte("AEIMQUYcgkosw048"))
-	var bsfdig_2bit HParser = In([]byte("AQgw"))
-	var base64_3 HParser = Repeat_n(bsfdig, 4)
-	var base64_2 HParser = Sequence(bsfdig, bsfdig, bsfdig_4bit, equals, nil)
-	var base64_1 HParser = Sequence(bsfdig, bsfdig_2bit, equals, equals, nil)
-	var base64 HParser = Sequence(Many(base64_3), Optional(Choice(base64_2, base64_1, nil)), nil)
-	document = Sequence(Whitespace(base64), Whitespace(End_p()), nil)
+	base64_3 := Repeat_n(bsfdig, 4)
+	base64_2 := Sequence(bsfdig, bsfdig, bsfdig_4bit, equals)
+	base64_1 := Sequence(bsfdig, bsfdig_2bit, equals, equals)
+
+	base64 := Sequence(
+		Many(base64_3),
+		Optional(
+			Choice(base64_2, base64_1),
+		),
+	)
+
+	return base64
 }
 
 func TestBase64(t *testing.T) {
-	init_parser()
+	base64Parser := base64_parser()
+	documentParser := Sequence(
+		Whitespace(base64Parser),
+		Whitespace(End_p()),
+	)
 
 	input := []byte("aGk=") // "hi"
-	result := CParse(document, input)
-	defer result.Free()
+	ast := Parse(documentParser, input)
 
-	// show result of parse actions
-	if result.r != nil && result.r.ast != nil {
-		Pprint(os.Stdout, result.r.ast, 0, 0)
-	} else {
-		t.Error("fail")
+	if ast == nil {
+		t.Fatal("failed to parse")
 	}
-}
-
-func TestBase64_2(t *testing.T) {
-	init_parser()
-
-	input := []byte("aGk=") // "hi"
-	ast := Parse(document, input)
 
 	fmt.Printf("%#v\n", ast)
 }
